2387ed366e155fd72abfdd5a1a6864a4296b643b
Philipeano <philipeano@gmail.com>
2017-03-08 12:01:36 +0100

    Update library.js

---
+++ app/library.js
@@ -0,0 +1,29 @@
+'use strict'
+
+module.exports = {
+
+ /* Find the average of three integers */
+ computeAverage: function(num1, num2, num3) {
+   var average = 0;
+   return average;
+ },
+ 
+ /* Find the factorial of an integer */
+ computeFactorial: function(num) {
+   var factorial = 1;
+   return factorial;
+ },
+
+ /* Convert a given Celcius temperature to Fahrenheit */
+ convertTempCtoF: function(cTemp) {
+   var fTemp = 0;
+   return fTemp;
+ },
+
+ /* Convert a given Fahrenheit temperature to Celcius */
+ convertTempFtoC: function(fTemp) {
+   var cTemp = 0;
+   return cTemp;
+ }
+
+}
