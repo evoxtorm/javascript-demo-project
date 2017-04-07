886b2d7fefa2b29617ea8bf55d0b214e7121ad09
Philipeano <philipeano@gmail.com>
2017-04-07 21:03:37 +0100

    Update tests.js

--- tests/tests.js
+++ tests/tests.js
@@ -1,9 +1,8 @@
 // testing code goes here
 
-'use strict'
+'use strict';
 
-var chai = require('chai');
-var assert = chai.assert;
+const assert = require('chai').assert;
 
 var myApp = require('../app/library.js');
 
@@ -21,16 +20,16 @@ describe("Arithmetic Functionality", function() {
 
 
 describe("Temperature Conversion Functionality", function() {
-  it("should return X for Celcius value Y", function() {
+  it("should return 104 for Celcius value 40", function() {
     assert(myApp.convertTempCtoF(40) == 104);
   })
-  it("should return X for Celcius value Y", function() {
+  it("should return 140 for Celcius value 60", function() {
     assert(myApp.convertTempCtoF(60) == 140);
   })
-  it("should return Y for Fahrenheit value X", function() {
+  it("should return 60 for Fahrenheit value 140", function() {
     assert(myApp.convertTempFtoC(140) == 60);
   })
-  it("should return Y for Fahrenheit value X", function() {
+  it("should return 40 for Fahrenheit value 104", function() {
     assert(myApp.convertTempFtoC(104) == 40);
   })
 })
